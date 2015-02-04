219284be28c3d22ab6552cc33c22cb816c7bd215
François de Campredon <francois.de.campredon@gmail.com>
2015-02-04 08:44:42 +0100

    improve test cases for type assertion + jsx

--- tests/cases/conformance/jsx/jsxAndTypeAssertion.ts
+++ tests/cases/conformance/jsx/jsxAndTypeAssertion.ts
@@ -1,6 +1,8 @@
 declare var React: any;
 
 class div {}
+class foo {}
+class bar {}
 
 
 var x: any;
@@ -15,3 +17,9 @@ x = <div>hello {<div>{}}</div>;
 x = <div test={<div>{}}>hello</div>;
 
 x = <div test={<div>{}}>hello {<div>{}}</div>;
+
+x = <foo>x</foo>, x = <img/>;
+
+<foo>{<foo><foo>{/foo/.test(x) ? <foo><foo></foo> : <bar><bar></bar>}</foo>}</foo>
+    
+    
